7e5ba9b7c0e00e0e91d886d951e268621233b923
Inseptentrio <kopriella@gmail.com>
2024-11-08 03:58:36 +0300

    Update change.js

--- Passport/change.js
+++ Passport/change.js
@@ -1,1 +1,11 @@
-
+const ChangeText = () =>
+  {
+  document.getElementsByClassName('block1')[0].textContent = 'GU OF THE MINISTRY OF INTERNAL AFFAIRS OF RUSSIA';
+  document.getElementsByClassName('block2')[0].textContent = 'IN ST. PETERSBURG AND THE LENINGRAD';
+  document.getElementsByClassName('block3')[0].textContent = 'REGION';
+  document.getElementsByClassName('block6')[0].textContent = 'STAROSTINA';
+  document.getElementsByClassName('block7')[0].textContent = 'ARINA';
+  document.getElementsByClassName('block8')[0].textContent = 'EUGENEVNA';
+  document.getElementsByClassName('block9')[0].textContent = 'FEM';
+  document.getElementsByClassName('block11')[0].textContent = 'IZHEVSK';
+  }
