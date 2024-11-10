c119460913330ccb5e01e2f247fb9d7338ed5dfe
Inseptentrio <kopriella@gmail.com>
2024-11-10 22:53:28 +0300

    Update latin.js

--- Passport/latin.js
+++ Passport/latin.js
@@ -1,9 +1,9 @@
 const changer = document.getElementById("button")
 
 function change_all(){   
-    if (document.getElementsByTagName('div')[0].innerText='GU OF THE MOIAOR';){  
+    if (document.getElementsByTagName('div')[0].innerText==='GU OF THE MOIAOR'){  
        
-        document.getElementsByTagName('div')[0].innerText='ГУ МВД РОССИИ ПО' 
+        document.getElementsByTagName('div')[0].innerText='ГУ МВД РОССИИ ПО';
         document.getElementsByTagName('div')[1].innerText='Г.САНКТ-ПЕТЕРБУРГУ И ЛЕНИНГРАДСКОЙ';
         document.getElementsByTagName('div')[2].innerText='ОБЛАСТИ';
         document.getElementsByTagName('div')[5].innerText='СТАРОСТИНА';
@@ -11,9 +11,9 @@ function change_all(){
         document.getElementsByTagName('div')[7].innerText='ЕВГЕНЬЕВНА';
         document.getElementsByTagName('div')[8].innerText='ЖЕН';
         document.getElementsByTagName('div')[10].innerText=' Г. &nbsp; ИЖЕВСК ';
-   }   
+    }   
    else{
-       if (document.getElementsByTagName('div')[0].innerText='ГУ МВД РОССИИ ПО';){  
+       if (document.getElementsByTagName('div')[0].innerText==='ГУ МВД РОССИИ ПО'){  
            
             document.getElementsByTagName('div')[0].innerText='GU OF THE MOIAOR';  
             document.getElementsByTagName('div')[1].innerText='IN ST. PETERSBURG AND THE LENINGRAD';
@@ -23,8 +23,8 @@ function change_all(){
             document.getElementsByTagName('div')[7].innerText='EUGENEVNA';
             document.getElementsByTagName('div')[8].innerText='FEM';
             document.getElementsByTagName('div')[10].innerText='IZHEVSK';
-       }   
-   }
+        }   
+    }
 }
 
 changer.addEventListener("click",change_all)
